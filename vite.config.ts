a10e4efebd4a38b537fb307d781792568c7bb233
ahmedkotb96 <ahmedabdelhakiem96@gmail.com>
2025-06-27 00:08:21 +0300

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig({
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: "/looklike",
+  base: command === "serve" ? "/" : "/looklike/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
